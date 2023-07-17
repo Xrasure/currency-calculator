163b17a35bf5b628eed7c833aa8079ffd87bb587
Stefanos Vakirtzis <stefanos.vakirtzis@factset.com>
2023-07-17 15:24:06 +0300

    installed sqlite and created database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,58 @@
 const express = require('express')
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+app.use(express.json());
+
+// /**
+//  * Return all available currencies
+//  */
+// app.get('/currency', (req, res) => {
+  
+//   // fetch all currencies from db
+//   //return currencies
+
+//   return res.status(200).json();
+// })
+
+
+/**
+ * Returns the currency with the provided id
+ */
+app.get('/currency/:id', (req, res) => {
+  // read id
+  // fetch currency from db
+  //return currency details
+
+  // req.params.id
+
+  return res.status(200).json('GET');
 })
+
+/**
+ * Create a new currency
+ */
+app.post('/currency', (req, res) => {
+
+  // read incoming currency from request
+  // create currency in DB
+  //return success
+
+  // req.body
+
+  const curr = req.body;
+  // name, symbol
+  // curr.name
+  
+
+  // dbNAme= currency-calculator
+  // tableName = currencies
+
+  return res.status(200).json('POST');
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
