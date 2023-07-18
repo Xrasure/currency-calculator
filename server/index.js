987d3d959cb4b1c9828ccd3c251c1ee2523fa1aa
AlexiD <alexios2005@protonmail.com>
2023-07-18 16:46:17 +0300

    added CRUD for exchanges

--- server/index.js
+++ server/index.js
@@ -96,7 +96,7 @@ app.post('/currency', (req, res) => {
 
 //UPDATE COMMANDS
 
-app.update('/currency/update/:id', (req, res) => {
+app.put('/currency/:id', (req, res) => {
   // req.body
 
   const curr = req.body;
@@ -147,6 +147,143 @@ app.delete('/currency/:id', (req, res) => {
   //new
   db.serialize(() => { 
     db.run("DELETE FROM currencies WHERE id = ?",id, (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json("error");
+      } else {
+        console.log('deleted successfully');
+        return res.status(200).json("deleted successfully");
+      }
+    });
+  });
+
+  
+
+
+})
+
+
+
+//ExchangeRates
+
+app.post('/exchange', (req, res) => {
+
+  // read incoming currency from request
+  // create currency in DB
+  //return success
+
+  // req.body
+
+  const curr = req.body;
+  // name, symbol
+  // curr.name
+
+
+  
+  db.serialize(() => {
+    // Create a table "Currencies" in Database
+    db.run('CREATE TABLE IF NOT EXISTS exchanges (id INTEGER PRIMARY KEY, currencyid INTEGER, exchangerate REAL)');
+  
+    // Insert data from post request into table
+    db.run('INSERT INTO exchanges (currencyid, exchangerate) VALUES (?, ?)', [curr.currencyid, curr.exchangerate], (result, err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json("error")
+      } else {
+        console.log('Row inserted successfully!');
+        return res.status(200).json("success");
+      }
+    });
+  });
+
+  
+
+
+})
+
+
+
+app.get('/exchange', (req, res) => {
+
+  db.all('SELECT * FROM exchanges', (err,data) => {
+    console.log({err,data})
+    if (err) {
+      return res.status(500).json("error");
+    }
+
+    return res.status(200).json(data);
+  });
+  // fetch all currencies from db
+  //return currencies
+
+  // return res.status(200).json();
+})
+
+
+
+
+app.get('/exchange/:id', (req, res) => {
+  var id=req.params.id;
+
+  return db.get('SELECT * FROM exchanges WHERE id  = ?',id, (err,data) => {
+    console.log({err,data})
+    if (err) {
+      return res.status(500).json("error");
+    }
+
+    return res.status(200).json(data);
+  });
+  // read id
+  // fetch currency from db
+  //return currency details
+
+  // req.params.id
+
+  return res.status(200).json('GET');
+})
+
+
+
+app.put('/exchange/:id', (req, res) => {
+  // req.body
+
+  const curr = req.body;
+  var id=req.params.id;
+  // name, symbol
+  // curr.name
+
+
+  //new
+  db.serialize(() => { 
+    db.run("UPDATE exchanges SET currencyid = ?, exchangerate = ? WHERE id = ?", [curr.currencyid, curr.exchangerate, id] , (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json("error");
+      } else {
+        console.log('Updated successfully!');
+        console.log(curr.name);
+        return res.status(200).json("updated successfully");
+      }
+    });
+  });
+
+  
+
+
+})
+
+
+app.delete('/exchange/:id', (req, res) => {
+
+
+  const curr = req.body;
+  var id=req.params.id;
+  // name, symbol
+  // curr.name
+
+
+  db.serialize(() => { 
+    db.run("DELETE FROM exchanges WHERE id = ?",id, (err) => {
       if (err) {
         console.error(err);
         return res.status(500).json("error");
