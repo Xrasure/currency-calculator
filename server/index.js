8efffa73896f085735839c6a788119a3876fdcbf
AlexiD <alexios2005@protonmail.com>
2023-07-18 17:03:50 +0300

    CRUD added for users

--- server/index.js
+++ server/index.js
@@ -300,6 +300,146 @@ app.delete('/exchange/:id', (req, res) => {
 })
 
 
+//USER
+app.post('/user', (req, res) => {
+
+  // read incoming currency from request
+  // create currency in DB
+  //return success
+
+  // req.body
+
+  const curr = req.body;
+  // name, symbol
+  // curr.name
+
+
+  
+  db.serialize(() => {
+    // Create a table "Currencies" in Database
+    db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)');
+  
+    // Insert data from post request into table
+    db.run('INSERT INTO users (username, password) VALUES (?, ?)', [curr.username, curr.password], (result, err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json("error")
+      } else {
+        console.log('Row inserted successfully!');
+        return res.status(200).json("success");
+      }
+    });
+  });
+
+  
+
+
+})
+
+
+
+app.get('/user', (req, res) => {
+
+  db.all('SELECT * FROM users', (err,data) => {
+    console.log({err,data})
+    if (err) {
+      return res.status(500).json("error");
+    }
+
+    return res.status(200).json(data);
+  });
+  // fetch all currencies from db
+  //return currencies
+
+  // return res.status(200).json();
+})
+
+
+
+
+app.get('/user/:id', (req, res) => {
+  var id=req.params.id;
+
+  return db.get('SELECT * FROM users WHERE id  = ?',id, (err,data) => {
+    console.log({err,data})
+    if (err) {
+      return res.status(500).json("error");
+    }
+
+    return res.status(200).json(data);
+  });
+  // read id
+  // fetch currency from db
+  //return currency details
+
+  // req.params.id
+
+  return res.status(200).json('GET');
+})
+
+
+
+app.put('/user/:id', (req, res) => {
+  // req.body
+
+  const curr = req.body;
+  var id=req.params.id;
+  // name, symbol
+  // curr.name
+
+
+  //new
+  db.serialize(() => { 
+    db.run("UPDATE users SET username = ?, password = ? WHERE id = ?", [curr.username, curr.password, id] , (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json("error");
+      } else {
+        console.log('Updated successfully!');
+        console.log(curr.name);
+        return res.status(200).json("updated successfully");
+      }
+    });
+  });
+
+  
+
+
+})
+
+
+app.delete('/user/:id', (req, res) => {
+
+
+  const curr = req.body;
+  var id=req.params.id;
+  // name, symbol
+  // curr.name
+
+
+  db.serialize(() => { 
+    db.run("DELETE FROM users WHERE id = ?",id, (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json("error");
+      } else {
+        console.log('deleted successfully');
+        return res.status(200).json("deleted successfully");
+      }
+    });
+  });
+})
+
+
+
+
+
+
+
+
+
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
